04cea71b8db9a93941dd8cd6f69b32f7abd8163e
Mutombo <mgtshomba@gmail.com>
2020-11-24 23:10:27 +0200

    Initial commit

---
+++ src/Components/Calendar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Calendar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Calendar Component</h3>
+            </div>
+        )
+    }
+}
+
+export default Calendar;
